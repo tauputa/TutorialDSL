52832c2f7d62f8a896ddf20dbf0bfb026512aa41
Alistair.Young <alistair.young@xero.com>
2022-08-28 17:13:10 +1200

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,12 +1,18 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.Swabra
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+
+version = "2021.1"
 
 project {
     params {
         param("teamcity.ui.settings.readOnly", "true")
     }
-    buildType(Build)
+//    buildType(Build)
 //    buildType(cleanFiles(agentRequirements(Build)))
-    
-//    buildType(cleanFiles(agentRequirements( Maven("Build","clean compile","-Dmaven.test.failure.ignore=true"))))
+    buildType(cleanFiles(agentRequirements( Maven("Build","clean compile","-Dmaven.test.failure.ignore=true"))))
 }
 
 class Maven (Name:String,Goals:String,RunnerArgs:String): BuildType({
