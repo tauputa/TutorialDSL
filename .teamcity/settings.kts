e0e80fdf87718c7157552b866eec9c3be7509919
Alistair.Young <alistair.young@xero.com>
2022-08-28 17:02:03 +1200

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -11,42 +11,21 @@ project {
         param("teamcity.ui.settings.readOnly", "true")
     }
 //    buildType(Build)
-    buildType(cleanFiles(agentRequirements(Build)))
+//    buildType(cleanFiles(agentRequirements(Build)))
+    buildType(Maven("Build","clean compile","-Dmaven.test.failure.ignore=true"))
 }
 
-object Build : BuildType({
-    id("Build")
-    name = "CleanTest"
+class Maven (Name:String,Goals:String,RunnerArgs:String): BuildType({
+    id(name)
+    this.name=Name
     vcs {
         root(DslContext.settingsRoot)
     }
     steps {
         maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            this.goals = Goals
+            this.runnerArgs = RunnerArgs
             mavenVersion = bundled_3_6()
         }
     }
-    triggers {
-        vcs {
-        }
-    }
 })
-
-fun agentRequirements(buildType: BuildType): BuildType{
-    buildType.requirements {
-        contains("teamcity.agent.name", "linux")
-        equals("aws.region", "ap-southeast-2")
-    }
-    return buildType
-}
-
-fun cleanFiles(buildType: BuildType): BuildType {
-    buildType.features {
-        swabra {
-            lockingProcesses = Swabra.LockingProcessPolicy.REPORT
-            verbose = true
-        }
-    }
-    return buildType
-}
