0c1723288123cf32a6adc35f38dc6862b8ac19b3
Alistair.Young <alistair.young@xero.com>
2022-08-29 06:35:55 +1200

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -12,7 +12,8 @@ project {
     }
 //    buildType(Build)
 //    buildType(cleanFiles(agentRequirements(Build)))
-    buildType(vcsTrigger(cleanFiles(agentRequirements( Maven("Build","clean compile","-Dmaven.test.failure.ignore=true")))))
+//    buildType(vcsTrigger(cleanFiles(agentRequirements( Maven("Build","clean compile","-Dmaven.test.failure.ignore=true")))))
+    buildType(vcsTrigger(Maven("Build","clean compile","-Dmaven.test.failure.ignore=true")))
 }
 
 class Maven (Name:String,Goals:String,RunnerArgs:String): BuildType({
@@ -29,34 +30,6 @@ class Maven (Name:String,Goals:String,RunnerArgs:String): BuildType({
         }
     }
 })
-
-fun vcsTrigger(buildType: BuildType): BuildType{
-    buildType.triggers {
-        vcs {
-            branchFilter = ""
-            enableQueueOptimization = false
-        }
-    }
-    return buildType
-}
-
-fun agentRequirements(buildType: BuildType): BuildType{
-    buildType.requirements {
-        contains("teamcity.agent.name", "linux")
-        equals("aws.region", "ap-southeast-2")
-    }
-    return buildType
-}
-
-fun cleanFiles(buildType: BuildType): BuildType {
-    buildType.features {
-        swabra {
-            lockingProcesses = Swabra.LockingProcessPolicy.REPORT
-            verbose = true
-        }
-    }
-    return buildType
-}
 
 fun vcsTrigger(buildType: BuildType): BuildType{
     if (buildType.triggers.items.find { it.type == "vcs" } == null ) {
